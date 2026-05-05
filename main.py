834afc7e8c64f0fd3b37b8ee723a33d58d9d8cf5
Vadym Priafun <pryadun.vv06@gmail.com>
2026-05-05 19:48:04 +0300

    Main file of program

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from PyQt5.QtWidgets import QApplication
 from app.database import init_db
 from app.ui import LoginWindow
 
-
-if __name__ == "__main__":
+def main():
+    # Ініціалізуємо БД
     init_db()
 
+    # Запускаємо PyQt застосунок
     app = QApplication(sys.argv)
-
     login = LoginWindow()
     login.show()
-
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
